bfc10c4236f6765a3279f3bfc442bc6038c46983
Jonathan Holland <jonholland@icloud.com>
2025-07-08 09:42:52 -0400

    Manual lint

--- Package.swift
+++ Package.swift
@@ -25,7 +25,6 @@ let package = Package(
 			name: "ComposableArchitecturePattern",
 			dependencies: []
 		),
-
         .testTarget(
             name: "ComposableArchitecturePatternTests",
             dependencies: [
