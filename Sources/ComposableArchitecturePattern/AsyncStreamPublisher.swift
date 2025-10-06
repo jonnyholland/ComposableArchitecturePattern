663a1312ae557ba82f0b675a25b3d697ac03316a
Jonathan Holland <jonathan@buttery.technology>
2025-10-05 23:02:14 -0400

    + AsyncStreamPublisher

---
+++ Sources/ComposableArchitecturePattern/AsyncStreamPublisher.swift
@@ -0,0 +1,38 @@
+//
+//  AsyncStreamPublisher.swift
+//  ComposableArchitecturePattern
+//
+//  Created by Jonathan Holland on 10/5/25.
+//
+
+/// A property wrapper for a value that can handle publishing streams.
+@propertyWrapper public struct AsyncStreamPublisher<Value: Sendable>: Sendable {
+	public var wrappedValue: Value {
+		didSet {
+			self._publish(self.wrappedValue)
+		}
+	}
+	
+	/// Create a new `AsyncStreamPublisher` with the specified value.
+	public init(wrappedValue: Value) {
+		self.wrappedValue = wrappedValue
+	}
+	
+	/// A new stream for the underlying value.
+	public mutating func newStream() -> AsyncStream<Value> {
+		AsyncStream { continuation in
+			self._streams.append(continuation)
+			continuation.yield(self.wrappedValue)
+		}
+	}
+	
+	/// The streams that have been created to listen to the underlying value.
+	private var _streams: [AsyncStream<Value>.Continuation] = []
+	
+	/// Publish the new value to the streams.
+	private func _publish(_ value: Value) {
+		for continuation in self._streams {
+			continuation.yield(value)
+		}
+	}
+}
