d045f18eede5503149d8927b79e0d648dd8f92fe
Jonny Holland <jonathan@buttery.technology>
2026-02-21 15:25:07 -0500

    Remove ViewCoordinatorState

--- Sources/ComposableArchitecturePattern/ViewCoordinator.swift
+++ Sources/ComposableArchitecturePattern/ViewCoordinator.swift
@@ -96,24 +96,3 @@ public protocol ViewCoordinator: Coordinator {
 	/// What the coordinator displays as its main content
 	var view: ViewCoordinatorContentView { get }
 }
-
-@available(*, renamed: "CoordinatorState")
-public enum ViewCoordinatorState: Equatable {
-	public static func == (lhs: ViewCoordinatorState, rhs: ViewCoordinatorState) -> Bool {
-		if case let .error(lhsError) = lhs, case let .error(rhsError) = rhs {
-			return lhsError?.localizedDescription == rhsError?.localizedDescription
-		} else if case .idle = lhs, case .idle = rhs {
-			return true
-		} else if case .loaded = lhs, case .loaded = rhs {
-			return true
-		} else if case .needsReload = lhs, case .needsReload = rhs {
-			return true
-		}
-		return false
-	}
-	
-	case idle
-	case loaded
-	case needsReload
-	case error(error: Error? = nil)
-}
