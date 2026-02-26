ce2fc870aae9d312573d793c2206a321775e5d1f
Jonny Holland <jonathan@buttery.technology>
2026-02-26 14:37:41 -0500

    Update Token

--- Sources/ComposableArchitecturePattern/Token.swift
+++ Sources/ComposableArchitecturePattern/Token.swift
@@ -7,13 +7,22 @@
 
 import Foundation
 
+/// A token object for authentication.
 public struct Token: Sendable {
+	/// The value of the token.
 	public var value: String?
+	/// When the token expires.
 	public var expiresOn: Date?
 
 	public init(value: String? = nil, expiresOn: Date? = nil) {
 		self.value = value
 		self.expiresOn = expiresOn
+	}
+	
+	/// Create a new token with the given value and expiration duration.
+	public init(value: String? = nil, expiresIn: TimeInterval) {
+		self.value = value
+		self.expiresOn = .init(timeIntervalSince1970: expiresIn)
 	}
 
 	/// Whether the token has expired based on the current date.
